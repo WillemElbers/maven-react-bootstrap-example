e4e61133a269fc78c7bca6c2d4b42dbb64e3fc00
Willem Elbers <willem@clarin.eu>
2015-11-10 15:00:55 +0100

    Added npm package.json and gulp gulpfile.js

---
+++ src/main/frontend/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var reactify = require('reactify'); 
+var concat = require('gulp-concat');
+ 
+gulp.task('browserify', function() {
+    var bundler = browserify({
+        entries: ['./app/main.jsx'],
+        transform: [reactify], 
+        debug: true,
+        cache: {}, packageCache: {}, fullPaths: true
+    });
+    bundler
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('./build/js/'));
+
+});
+    
+gulp.task('css', function () {
+    gulp
+        .src('styles/**/*.css')
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('html', function() {
+    gulp
+        .src(['index.html'])
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('default', ['browserify', 'css', 'html']);
